df2ec51d963aadd6392abefe0875def141eead4d
kalyan <kalyankarthi11@gmail.com>
2020-12-02 07:54:51 +0530

    ts-compiler and config

--- initialproject/basics.ts
+++ initialproject/basics.ts
@@ -1,4 +1,4 @@
-function add(n1:number, n2:number, showResult: boolean, phrase: string){
+function addB(n1:number, n2:number, showResult: boolean, phrase: string){
     if(typeof n1 !== 'number' || typeof n2 !== 'number'){
         throw new Error('Incorrect input');[]
     }
@@ -13,7 +13,7 @@ function add(n1:number, n2:number, showResult: boolean, phrase: string){
 
 const number1=4;
 const number2=2.5;
-const printResult = true;
+const printResultVar = true;
 const resultPhrase = 'Result is: '
 
-add(number1, number2, printResult, resultPhrase);
+addB(number1, number2, printResultVar, resultPhrase);
